f04fc16e53543ecbfa8329c3d33542b1bdcc51ee
Son Anthony Estomagulang <120387951+r111n@users.noreply.github.com>
2024-02-03 18:52:34 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/r111n-portfolio /"
+  base: "/r111n-portfolio/"
 })
